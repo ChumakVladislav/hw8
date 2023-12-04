b5ec8100108346e5d56556e4f677fa3506007dca
ChumakVladislav <chumakvolv@gmail.com>
2023-12-04 22:30:10 +0200

    push 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,6 +1,8 @@
 // const vimeoPlayer = require('@vimeo/player');
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
+import wait from 'lodash.throttle';
+
 const iframe = document.querySelector('iframe');
 
 const player = new Player(iframe);
